14e55fc847579300660231d7d9d945d3a9491ae6
HuskyHsu <chessman416@hotmail.com>
2017-11-12 17:27:21 +0800

    modify upload file method

--- uploadFile/views.py
+++ uploadFile/views.py
@@ -6,18 +6,35 @@ from django.db import connection
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    field = [
+        {
+            "name": "Capa2",
+            "fileName": "Capa2_final_storage_1.dat"
+        },
+        {
+            "name": "Capa3",
+            "fileName": "Capa3_final_storage_1.dat"
+        },
+        {
+            "name": "Capa4",
+            "fileName": "capa4new_Table1.dat"
+        },
+    ]
+
+    return render(request, 'index.html', {"field": field})
 
 def post_file(request):
     # POST and have file
     if request.method == 'POST' and request.FILES != {}:
 
-        site = LHC.Site().create(request.POST['site'])
-        success = site.readFile(request.FILES['file']).insert()
-        
+        success = {}
+        for file in request.FILES.keys():
+
+            site = LHC.Site().create(file)
+            success[file] = site.readFile(request.FILES[file]).insert()
+
         return JsonResponse({
                 'success': success,
-                'data': site.data,
                 })
 
     else:
