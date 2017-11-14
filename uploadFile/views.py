d1ad19dc6b2b9e4a831dff7cb97a6e76467bbf35
HuskyHsu <chessman416@hotmail.com>
2017-11-15 00:14:34 +0800

    async test ing

--- uploadFile/views.py
+++ uploadFile/views.py
@@ -4,6 +4,11 @@ from .process import LHC
 
 from django.db import connection
 
+import asyncio
+import time
+
+now = lambda: time.time()
+
 # Create your views here.
 def index(request):
     field = [
@@ -27,14 +32,31 @@ def post_file(request):
     # POST and have file
     if request.method == 'POST' and request.FILES != {}:
 
+        async def test01(file, FILES, date):
+            site = LHC.Site().create(file)
+            site.readFile(FILES, date)
+            return await site.insert()
+
+        start = now()
         success = []
+        tasks = []
+
+        # loop = asyncio.new_event_loop()
+        # asyncio.set_event_loop(loop)
         for file in request.FILES.keys():
 
+            # coroutine = test01(file, request.FILES[file], request.POST["date"])
+            # tasks.append( asyncio.ensure_future(coroutine) )
             site = LHC.Site().create(file)
             site.readFile(request.FILES[file], request.POST["date"])
-
             success.append({ "name": file, "result": site.insert()})
+        
+        # loop.run_until_complete(asyncio.wait(tasks))
 
+        # for task in tasks:
+        #     print('Task ret: ', task.result())
+
+        print('TIME: ', now() - start)
         return render(request, 'checkPage.html', {"success": success, "date": request.POST["date"] })
         # return JsonResponse({
         #         'success': success,
