d4d58aea7e17166be65a810a0eb0e2a79f0681cd
HuskyHsu <chessman416@hotmail.com>
2017-12-07 21:45:26 +0800

    add time series line

--- home/static/js/viewData.js
+++ home/static/js/viewData.js
@@ -4,8 +4,8 @@ var app = new Vue({
     data: {
         site: 'Capa2',
         item: '',
-        startTime: '2017-11-01',
-        endTime: '2017-11-10',
+        startTime: '2015-04-01',
+        endTime: '2015-06-10',
         siteBasic: [],
         timeSeries: []
     },
@@ -33,7 +33,67 @@ var app = new Vue({
                     .then(function (response) {
                         if (response.data.check) {
                             // console.log(response);
-                            vm.timeSeries = response.data.data;
+                            // vm.timeSeries = response.data.data;
+                            var data = response.data.data;
+
+                            var svg = d3.select("svg");
+                            var margin = { top: 20, right: 20, bottom: 30, left: 50 };
+                            var width = +svg.attr("width") - margin.left - margin.right;
+                            var height = +svg.attr("height") - margin.top - margin.bottom;
+                            svg.selectAll("g").remove();
+                            var g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+                            var parseTime = d3.timeParse("%Y-%m-%dT%H:%M:%S");
+                            // 2015-05-10T05:30:00
+                            var x = d3.scaleTime()
+                                .rangeRound([0, width]);
+
+                            var y = d3.scaleLinear()
+                                .rangeRound([height, 0]);
+
+                            var line = d3.line()
+                                .x(function (d) {
+                                    return x(parseTime(d.TIMESTAMP));
+                                })
+                                .y(function (d) {
+                                    return y(d[item]);
+                                });
+
+                            x.domain(d3.extent(data, function (d) {
+                                return parseTime(d.TIMESTAMP);
+                            }));
+                            y.domain(d3.extent(data, function (d) {
+                                return d[item];
+                            }));
+
+                            g.append("g")
+                                .attr("transform", "translate(0," + height + ")")
+                                .call(d3.axisBottom(x))
+                                .select(".domain")
+                                .remove();
+
+                            g.append("g")
+                                .call(d3.axisLeft(y))
+                                .append("text")
+                                .attr("fill", "#000")
+                                .attr("transform", "rotate(-90)")
+                                .attr("y", 6)
+                                .attr("dy", "0.71em")
+                                .attr("text-anchor", "end")
+                                .text(item);
+
+                            g.append("path")
+                                .datum(data)
+                                .attr("fill", "none")
+                                .attr("stroke", "steelblue")
+                                .attr("stroke-linejoin", "round")
+                                .attr("stroke-linecap", "round")
+                                .attr("stroke-width", 1.5)
+                                .attr("d", line);
+
+
+
+
                         }
                     })
                     .catch(function (error) {
