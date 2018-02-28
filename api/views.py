176a7e2fc667dfa8c0ee4171c4ad76042743880c
HuskyHsu <chessman416@hotmail.com>
2018-02-28 22:08:11 +0800

    paper CRUD

--- api/views.py
+++ api/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from database.models import WorkExperience
+from database.models import WorkExperience, Papers
 
-
-# Create your views here.
+# 工作經歷
 def work_experience(request):
 
     if request.method == 'POST':
@@ -17,14 +16,13 @@ def work_experience(request):
                 end_year = None if request.POST["end_Year"] == "" else int(request.POST["end_Year"])
             )
         elif request.POST["type"] == 'modify':
-            
             WorkExperience.objects.filter(pid=request.POST["pid"]).update(
                     unit = request.POST["unit"],
                     department = request.POST["department"],
                     title = request.POST["title"],
                     start_year = int(request.POST["start_Year"]),
                     end_year = None if request.POST["end_Year"] == "" else int(request.POST["end_Year"])
-                )
+            )
 
         elif request.POST["type"] == 'delete':
             WorkExperience.objects.filter(pid=request.POST["pid"]).delete()
@@ -34,3 +32,28 @@ def work_experience(request):
 
     return HttpResponseRedirect('/CMS/work_experience')
 
+# 代表著作
+def papers(request):
+
+    if request.method == 'POST':
+
+        if request.POST["type"] == 'add':
+            Papers.objects.create(
+                author = request.POST["author"],
+                date = request.POST["date"],
+                title = request.POST["title"]
+            )
+        elif request.POST["type"] == 'modify':
+            Papers.objects.filter(pid=request.POST["pid"]).update(
+                author = request.POST["author"],
+                date = request.POST["date"],
+                title = request.POST["title"]
+            )
+
+        elif request.POST["type"] == 'delete':
+            Papers.objects.filter(pid=request.POST["pid"]).delete()
+
+    else:
+        print('get')
+
+    return HttpResponseRedirect('/CMS/papers')
