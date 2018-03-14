aaa5bf8f407c3d6112bbb2c6f2e28a4ff577e588
HuskyHsu <chessman416@hotmail.com>
2018-03-14 23:42:11 +0800

    del old uploadfile app

--- uploadFile/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^file$', views.post_file, name='upload_post_file'),
-    url(r'$', views.index, name='upload_index'),
-    # url(r'^admin/', admin.site.urls),
-]
